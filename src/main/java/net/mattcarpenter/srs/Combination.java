9b65340f24f9f6631bb9184cf21c9d06d04e0761
Matt Carpenter <matt@Matts-MacBook-Pro.local>
2019-12-15 09:18:01 +0900

    add remaining test classes

--- src/main/java/net/mattcarpenter/srs/Combination.java
+++ src/main/java/net/mattcarpenter/srs/Combination.java
@@ -1,4 +1,9 @@
 package net.mattcarpenter.srs;
 
+import lombok.Builder;
+
+@Builder
 public class Combination {
+    private Knol front;
+    private Knol back;
 }
---
+++ src/test/java/net/mattcarpenter/srs/CardState.java
@@ -0,0 +1,4 @@
+package net.mattcarpenter.srs;
+
+public class CardState {
+}
